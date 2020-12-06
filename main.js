494cb7a183abc25b585ef62cbbd82cdf39cfce99
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-06 00:43:20 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -6,17 +6,13 @@ const FULL_HEART = '♥'
 document.addEventListener("DOMContentLoaded", () => {
   let errorModal = document.querySelector("#modal");
   errorModal.hidden = true;
-  heartListener()
+  document.querySelectorAll(".like-glyph").addEventListener("click", event => {
+    mimicServerCall("url")
 }) //end of dom content loaded listener
 //when someone clicks a heart, modal appears and set ti "Recognizing events"
 //when someone clicks heart, invoke mimicServerCall
 
 
-function heartListener() {
-  document.querySelectorAll(".like-glyph").addEventListener("click", event => {
-    mimicServerCall("url")
-  })
-}
 
 function modalHandling() { //if server returns error, show modal
   mimicServerCall("url")
